cf705492f92087668db9064540245e4bcd77cd91
Alexander Frosell <sachafrosell@gmail.com>
2018-10-05 16:51:47 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 require "pry"
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
-    binding.pry
+    if data[season][contestant][]
 end
 get_first_name_of_season_winner(data, "season 19")
 
